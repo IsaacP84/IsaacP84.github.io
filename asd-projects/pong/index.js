4a37675a261031c1e70f669973fbb1b726c71016
IsaacP84 <isaacpolly84@gmail.com>
2022-06-20 16:59:44 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,9 +10,36 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+
+  const WIDTH = $("#board").width;
+  const HEIGHT = $("#board").height;
   
   // Game Item Objects
-
+  var [ball, paddle1, paddle2] = [
+    {
+      id: "ball",
+      speed: 1,
+      //gets a random angle between 0 and 2pi
+      angle: Math.random() * (2*Math.PI)
+    },
+    {
+      id: "paddle1",
+      width: 10,
+      height: 100,
+      x: 0,
+      y: 0,
+      speedY: 0
+    },
+    {
+      id: "paddle2",
+      width: 10,
+      height: 100,
+      x: 0,
+      y: 0,
+      speedY: 0
+    }
+  ];
+  //have this definition make the objects on screen
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
